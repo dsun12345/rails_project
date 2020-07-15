1c16b10fad5533254238b06e7a04148f36701da6
danielsun <danielsun12345@gmail.com>
2020-07-14 17:00:21 -0700

    migrations

---
+++ db/migrate/20200714235715_create_songs.rb
@@ -0,0 +1,10 @@
+class CreateSongs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :songs do |t|
+      t.integer :user_id
+      t.integer :playlist_id
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+  end
+end
