e3c13d6c17a953f701b3a1c3a1c94131a0ba756b
danielsun <danielsun12345@gmail.com>
2020-07-14 21:49:58 -0700

    fixed user issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
     validates :email, presence: true
     validates :age, numericality: { only_integer: true}
     validates :password, length: { in: 6...20 }
+
 end 
--- db/schema.rb
+++ db/schema.rb
@@ -28,11 +28,9 @@ ActiveRecord::Schema.define(version: 2020_07_14_235715) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email"
     t.integer "age"
     t.string "password_digest"
-    t.integer "happiness"
-    t.integer "sleepiness"
-    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
